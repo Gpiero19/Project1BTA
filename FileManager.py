e6777bdb5e5920f6d6c141d07cf47e3508cc14c5
Gpiero19 <162220869+Gpiero19@users.noreply.github.com>
2024-03-28 11:45:49 +0100

    Add files via upload

---
+++ FileManager.py
@@ -0,0 +1,40 @@
+import json
+
+class FileManager:
+  def load_data(self, filename):
+    with open(filename, "r") as file:
+      content = json.load(file)
+    return content.text
+      # TODO:
+      # Implement a process that reads the contents of the `filename` file 
+      # and returns the text.
+
+  def save_data(self, filename, data):
+    with open(filename, "w") as file:
+      json.dump(data, file)
+      # TODO:
+      # Implement a process that writes the contents of `data` to the file `filename`
+
+  def read_json(self, json_file_path):
+    with open(json_file_path, "r") as file:
+      data = json.load(file)
+    return data
+          
+      # TODO:
+      # Implement a process that reads the contents of a file whose path is stored in the `json_file_path` variable 
+      # and returns a list of dictionaries
+      
+  def write_json(self, list_of_dicts, json_file_path):
+    with open(json_file_path, "w") as file:
+      json.dump(list_of_dicts, file)
+      #indent="\t"
+      # TODO:
+      # Implement a process that writes a list of dictionaries from list_of_dicts to the `json_file_path` file
+
+  def add_to_json(self, data, json_file_path):
+    with open(json_file_path, "a") as f:
+      json.dump(data, f)
+
+      pass
+      # TODO:
+      # Implement a process that gets the dictionary in the data variable and adds it to the JSON `json_file_path`
